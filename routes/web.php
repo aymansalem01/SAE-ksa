f0ea23bc1a79a7e2884fb69ad413aa9327048eac
aymansalem01 <aymansalem77777@gmail.com>
2025-10-06 13:13:02 +0300

    [edit] delete test page

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,3 @@ Route::get('/',[PageController::class,'index'])->name('index');
 
 Route::post('submission',[PageController::class,'submission'])->name('submission');
 Route::get('english', [PageController::class, 'english'])->name('english');
-Route::view('test', 'test')->name('test');
